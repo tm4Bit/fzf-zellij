fc57e9ccd1f92f7cd0eb5c8a71f201618f6cad18
Tiago Araujo <tiagomuniz130@gmail.com>
2023-11-08 17:33:12 -0300

    fix: change plugin dirname

--- init.plugin.zsh
+++ init.plugin.zsh
@@ -2,6 +2,6 @@
 
 if ! [ -x $HOME/.local/bin/fzj ]; then
   mkdir -p $HOME/.local/bin/
-  ln -s $HOME/.local/share/zap/plugins/power_zellij/fzj.sh $HOME/.local/bin/fzj
+  ln -s $HOME/.local/share/zap/plugins/fzf-zellij/fzj.sh $HOME/.local/bin/fzj
 fi
 
